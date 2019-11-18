4fd75be5845b45e07076120ca6ba34b94bcd4790
Addison Hill <adddison.hill@gmail.com>
2019-11-18 11:48:44 -0600

    create react app

---
+++ sleep-savy/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Sleep Savy is the best</h1>
+    </div>
+  );
+}
+
+export default App;
