486cbd9d5bf76cde73bba7317e1cb90aef0c78a6
Celestia Salinas <csalinas3671@gmail.com>
2019-11-18 16:38:42 -0500

    pt 2

--- sleep-savy/src/components/SignUp.js
+++ sleep-savy/src/components/SignUp.js
@@ -4,54 +4,41 @@ import * as Yup from "yup";
 import axios from "axios";
 import styled from "styled-components"; 
 
-const Wrapper = styled.section`
-  padding: 4em;
-  background: papayawhip;
-`
+
 
 function SignUp({ errors, touched, status }) {
-    const [users, setUsers] = useState([]);
+    const [users, setUsers] = useState({
+        username:"",
+        password:""
+    });
   
     useEffect(() => {
       status && setUsers(users => [...users, status]);
     }, [status]);
   
     return (
-        <Wrapper>
+ 
       <div>
         <Form>
-          <Field type="text" name="name" placeholder="Name" />
-          {touched.name && errors.name && <p className="errors">{errors.name}</p>}
-          <Field type="text" name="email" placeholder="Email" />
-          {touched.email && errors.email && (
-            <p className="errors">{errors.email}</p>
-          )}
+          <Field type="text" name="username" placeholder="Username" />
+          {touched.username && errors.username && <p className="errors">{errors.username}</p>}
           <Field type="password" name="password" placeholder="Password" />
           {touched.password && errors.password && (
             <p className="errors">{errors.password}</p>
           )}
-          <Field type="checkbox" name="terms" />
           <button type="submit">Submit</button>
         </Form>
-        {users.map(user => (
-          <ul key={user.id}>
-            <li>Name: {user.name}</li>
-            <li>Email: {user.email}</li>
-          </ul>
-        ))}
       </div>
-      </Wrapper>
+
     );
   }
   
   
   const FormikSignUpForm = withFormik({
-    mapPropsToValues({ name, email, password, terms }) {
+    mapPropsToValues({ name, password }) {
       return {
         name: name || "",
-        email: email || "",
         password: password || "",
-        terms: terms || false
       };
     },
   
@@ -59,16 +46,13 @@ function SignUp({ errors, touched, status }) {
       name: Yup.string()
         .min(3)
         .required("We need a name!"),
-      email: Yup.string()
-        .email()
-        .required("We need your email!"),
       password: Yup.string()
         .min(6)
         .required("Password, please!")
     }),
     handleSubmit(values, { setStatus, resetForm }) {
       axios
-        .post("https://reqres.in/api/users/", values)
+        .post("#", values)
         .then(res => {
           setStatus(res.data);
           console.log(res.data);
