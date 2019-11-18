1bb32dacc64c8f8e939bd40743471286c78e4868
brittanymae01 <brittanymae01>
2019-11-18 13:17:47 -0800

    login in form created

--- sleep-savy/src/App.js
+++ sleep-savy/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import Login from './components/Login'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Sleep Savy is the best</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <h1>Sleep Savy is the best</h1>
+        <Route exact path="/" render={props => <Login {...props} />} />
+      </div>
+    </Router>
   );
 }
 
---
+++ sleep-savy/src/components/Login.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import axios from 'axios'
+
+
+function Login(props) {
+    const [user, setUser] = useState({ username: '', password: '' })
+
+    const handleChange = event => {
+        setUser({ ...user, [event.target.name]: event.target.value });
+    };
+
+    const handleSubmit = e => {
+        axios
+            .post('https://sleepsavy.herokuapp.com/api/auth/login', user)
+            .then(response => {
+                console.log(response)
+                e.preventDefault();
+                localStorage.setItem('token', response.data.payload)
+                props.history.push('/')
+            })
+            .catch(error => {
+                console.log(error.response)
+            })
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label> Username
+                    <input
+                        type='text'
+                        name='username'
+                        value={user.username}
+                        onChange={handleChange}
+                    />
+                </label>
+                <label> Password
+                    <input
+                        type='password'
+                        name='password'
+                        value={user.password}
+                        onChange={handleChange}
+                    />
+                </label>
+            </form>
+            <button type='submit'>Submit</button>
+        </div>
+    )
+}
+
+export default Login
