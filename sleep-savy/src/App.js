1ea764a8cb47a07d0c5a80cc4118158adce2886d
Addison Hill <adddison.hill@gmail.com>
2019-11-19 17:27:27 -0600

    minor tweak to App.js

--- sleep-savy/src/App.js
+++ sleep-savy/src/App.js
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import PrivateRoute from './components/PrivateRoute';
-// import Login from './components/Login';
-// import Registration from './components/Registration';
 import Home from './components/Home';
 import AddForm from './components/AddForm';
 import EditForm from './components/EditForm';
@@ -16,18 +14,11 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <nav className="nav">
-          {/* <Link className="link" to="/">Login</Link>
-          <Link className="link" to="/registration">Registration</Link> */}
-          <Link className="link" to="/sleep">Home</Link>
-        </nav>
         <Switch>
-          {/* <Route exact path="/" render={props => <Login {...props} />} />
-          <Route path="/registration" render={props => <Registration {...props} /> } /> */}
           <PrivateRoute path="/sleep" component={Home} />
           <Route path="/add" render={props => <AddForm {...props} />} />
           <Route path="/edit/:id" render={props => <EditForm {...props} /> } />
-          {/* <Route component={Login} /> */}
+          <Route component={Login} />
         </Switch>
       </div>
     </Router>
